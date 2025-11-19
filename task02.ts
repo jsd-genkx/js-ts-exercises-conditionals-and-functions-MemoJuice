c2639243b9fba67fc1801a441aec404825c63f54
MemoJuice <Wrpxdouble1135@gmail.com>
2025-11-19 17:18:07 +0700

    feat: Update task 1,2

--- task02.ts
+++ task02.ts
@@ -1,5 +1,9 @@
 // TODO-02: Use if/else to check if it's a number
 // (hint: typeof or isNaN)
-
+;
 const userInput = "42";
 // Try changing to "hello", 10, null, NaN
+if (typeof userInput === "number" && !isNaN(userInput)) {
+    console.log(`True: ${userInput} This is a number`);
+}else
+    console.log(`False: ${userInput} This is not numer/ is NaN`);
